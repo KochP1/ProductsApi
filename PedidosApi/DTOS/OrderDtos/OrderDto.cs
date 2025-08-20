0ee0a1ba280dbe01fceaad4eed0ba544459e982d
Juan Koch <juanandreskochp@gamil.com>
2025-08-20 10:27:56 -0400

    Fixing nullable

--- PedidosApi/DTOS/OrderDtos/OrderDto.cs
+++ PedidosApi/DTOS/OrderDtos/OrderDto.cs
@@ -11,7 +11,7 @@ namespace PedidosApi.DTOS.OrderDtos
 
         public decimal TotalAmount { get; set; }
 
-        public string? Status { get; set; }
+        public string Status { get; set; }
 
         public string ShippingAddress { get; set; } = null!;
         public virtual CustomerDto Customer { get; set; } = null!;
